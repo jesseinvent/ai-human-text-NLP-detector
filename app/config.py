11bed07e6d6d797d6d7859d037e828a656e93126
Jesse Ojih <jcinvent05@gmail.com>
2026-03-11 04:53:17 +0100

    Updates

--- app/config.py
+++ app/config.py
@@ -5,4 +5,4 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 
 class Config:
     MODELS_PATH =  os.path.join(BASE_DIR, "models")
-    DATASET_PATH = os.path.join(BASE_DIR, "dataset", "ai_human_text_classification.csv")
+    DATASET_PATH = os.path.join(BASE_DIR, "dataset", "ai_human_text_dataset_2.xlsx")
